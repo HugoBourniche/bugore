481c2eff95564f578df92e30f7097bdbd2983d0b
hugobourniche <bourniche.hugo@gmail.com>
2025-05-02 11:32:36 +0200

    [FTR-03] Load tiles from JSON file

--- src/Home/Content/Content.tsx
+++ src/Home/Content/Content.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './Content.module.less';
 import TilesContainer from './TilesContainer/TilesContainer.tsx';
 import { TilesJsonObject } from '../../model/tiles-json-object.ts';
@@ -8,19 +8,30 @@ interface ContentProps {}
 
 const template = (tiles: TilesJsonObject | null) => (
   <>
-    <div className={styles.Content + ' text-size-m text-center'}>
+    <div className={`${styles.Content} text-size-m text-center`}>
       Site en cours de construction
     </div>
     {tiles && tiles.gameTiles && <TilesContainer title={'Jeux'} />}
+    {tiles && tiles.videoTiles && <TilesContainer title={'Videos'} />}
   </>
 );
 
 const Content: FC<ContentProps> = () => {
-  let tiles = null;
-  ReadJsonUtils.readTiles().then((json: any) => {
-    tiles = ReadJsonUtils.convertTilesJsonObject(json);
-    console.log(tiles);
-  });
+  const [tiles, setTiles] = useState<TilesJsonObject | null>(null);
+
+  useEffect(() => {
+    const fetchTiles = async () => {
+      try {
+        const json = await ReadJsonUtils.readTiles();
+        const convertedTiles = ReadJsonUtils.convertTilesJsonObject(json);
+        setTiles(convertedTiles);
+      } catch (error) {
+        console.error('Error when reading tiles: ' + error);
+      }
+    };
+    fetchTiles();
+  }, []);
+
   return template(tiles);
 };
 
